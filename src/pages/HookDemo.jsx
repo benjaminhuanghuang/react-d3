d3dadb9df6e723b5eb2ef4cf8e8b14bf1b253f02
benjaminhuanghuang <bhuang@rms.com>
2020-12-05 18:59:39 -0800

    hook demo

--- src/pages/HookDemo.jsx
+++ src/pages/HookDemo.jsx
@@ -1,20 +1,27 @@
-import React, {useRef, useEffect} from 'react'
-import {select} from 'd3';
+import React, { useRef, useEffect } from "react";
+import { select } from "d3";
 
-
-
-const data = [15, 4, 39,111]
+const data = [15, 4, 39, 111];
 
 export default function HookDemo() {
   const svgRef = useRef();
-  useEffect(()=>{
+  useEffect(() => {
     const svg = select(svgRef.current);
-    svg.selectAll('circle').data(data).join();
-  }, [])
+    svg
+      .selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => i * 70)
+      .attr("y", 0)
+      .attr("width", 25)
+      .attr("height", (d, i) => d)
+      .attr("fill", "green");
+  }, []);
   return (
     <>
-    <h1>Hook SVG</h1>
-    <svg ref={svgRef}></svg>
+      <h1>Hook SVG</h1>
+      <svg ref={svgRef}></svg>
     </>
-  )
+  );
 }
